87b87192ece4b127a1029e8efb04d33483b361ac
RgoSL <125403873+RgoSL@users.noreply.github.com>
2024-02-28 23:01:13 -0300

    Add files via upload

---
+++ Exercicio03.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}
+/* fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}*/
